0d8d87d9933e3e96b86802132d1768af0f568dc1
Gregor1987 <110369844+Gregor1987@users.noreply.github.com>
2022-12-07 09:15:26 +0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -15,7 +15,7 @@ class Horo:
     zodiac = ''
     period = ''
     keys = {'aries': '♈ Овен', 'taurus': '♉ Телец', 'gemini': '♊ Близнецы', 'cancer': '♋ Рак', 'leo':  '♌ Лев',
-            'virgo': '♍ Дева', 'libra': '♎ Весы', 'scorpius': '♏ Скорпион', 'sagittarius': '♐ Стрелец',
+            'virgo': '♍ Дева', 'libra': '♎ Весы', 'scorpio': '♏ Скорпион', 'sagittarius': '♐ Стрелец',
             'capricorn': '♐ Козерог', 'aquarius': '♒ Водолей', 'pisces': '♒ Рыбы'}
 
 
